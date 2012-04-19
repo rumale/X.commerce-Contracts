d3ad058b42b9476f3736c8fe2682b4be4fde3ec4
rumale <rumale@d93500b4-c195-437a-bfe3-bb401f81fdac>
2012-04-19 03:08:27 +0000

    made generic for xocl release as well

--- Tools/svnrelease.sh
+++ Tools/svnrelease.sh
@@ -1,60 +1,59 @@
 #!/bin/bash
 
-###########################################################
-## This script is to automate ontology release process   ##
-## It will perform following steps                       ##
-## 1. verify compilation                                 ##
-## 2. Tag for release                                    ##
-## 3. build and deploy release to nexus                  ##
-## Assumes svn/maven installation and maven credentials  ##
-## to be set for deployment. If unsure run using Jenkins ##
-###########################################################
+##########################################################################
+## This script is to automate ontology/xocl/incubator release process   ##
+## It will perform following steps                                      ##
+## 1. verify compilation                                                ##
+## 2. Tag for release                                                   ##
+## 3. build and deploy release to nexus                                 ##
+## Assumes svn/maven installation and maven credentials                 ##
+## to be set for deployment. If unsure run using Jenkins                ##
+##########################################################################
 
 usage()
 {
 cat << EOF
-usage: $0 [required-options] [optional-options]
+usage: $0 [options]
 
 OPTIONS:
    -h      show this message   
    -u      svn userid
    -p      svn password
-   -r      svn repository url <optional>
-   -t      svn tagbase <optional>
-Example: $0 -u johndoe -p secret -r https://svn.corp.x.com:8080/svn/x/Ontology/trunk/ -t https://svn.corp.x.com:8080/svn/x/Ontology/trunk/
+   -r      can be either of ontology/xocl/xocl-incubator
+Example: $0 -u johndoe -p secret -r xocl
 EOF
 }
 
 verify_install() {
-    rm -rf OntologyTrunk
-	svn co $REPO OntologyTrunk
-	cd OntologyTrunk
+    rm -rf "${PROJ}_trunk"
+	svn co $REPO/trunk "${PROJ}_trunk"
+	cd "${PROJ}_trunk"
 	mvn install
 	cd ..
 }
 
 release() {
-    rm -rf OntologyTrunkForTagging
-	svn co --trust-server-cert  --non-interactive $REPO OntologyTrunkForTagging
-	cd OntologyTrunkForTagging
+    rm -rf "${PROJ}_trunk_for_tagging"
+	svn co --trust-server-cert  --non-interactive $REPO/trunk "${PROJ}_trunk_for_tagging"
+	cd "${PROJ}_trunk_for_tagging"
 	set +x 
 	mvn --batch-mode release:prepare -Dusername=$SVN_USER -Dpassword=$SVN_PASS
 	set -x
 	cd ..
 }
 
-build_deploy() {
-	rm -rf OntologyTrunkTagged
-	str=`svn --trust-server-cert  --non-interactive log $TAGBASE --limit 1| grep ontology`
-	tag=${str##*ontology-}
-	RELEASE_TAG_URL=https://svn.corp.x.com:8080/svn/x/Ontology/tags/ontology-$tag
-	svn co $RELEASE_TAG_URL OntologyTrunkTagged
-	cd OntologyTrunkTagged
+build_deploy() {	
+	str=`svn --trust-server-cert --non-interactive ls $REPO/tags| sort -V | tail -1`
+	tag="${str%?}"
+	RELEASE_TAG_URL="${REPO}/tags/${tag}"	
+	rm -rf "${PROJ}_trunk_tagged"
+	svn co $RELEASE_TAG_URL "${PROJ}_trunk_tagged"
+	cd "${PROJ}_trunk_tagged"
 	mvn install deploy
 	cd ..
 }
 
-while getopts u:p:hrt OPTION
+while getopts u:p:r:h OPTION
 do
      case $OPTION in
          h)
@@ -68,10 +67,7 @@ do
              SVN_PASS=$OPTARG
              ;;
          r)
-             REPO=$OPTARG
-             ;;
-         t)
-             TAGBASE=$OPTARG
+             PROJ=$OPTARG
              ;;
          ?)
              usage
@@ -80,23 +76,25 @@ do
      esac
 done
 
-if [[ -z "$SVN_USER" ]] || [[ -z "$SVN_PASS" ]]
+ONTOLOGY="ontology"
+XOCL="xocl"
+INCUBATOR="xocl-incubator"
+
+if [[ -z "$SVN_USER" ]] || [[ -z "$SVN_PASS" ]] || [[ -z "$PROJ" ]]
 then
      usage
      exit 1
 fi
 
-if [[ -z "$REPO" ]]
+if [[ "$PROJ" != "ontology" ]] && [[ "$PROJ" != "xocl" ]] && [[ "$PROJ" != "xocl-incubator" ]]
 then
-	REPO=https://svn.corp.x.com:8080/svn/x/Ontology/trunk/
+	usage
+	exit 1
 fi
-
-if [[ -z "$TAGBASE" ]]
-then
-	TAGBASE=https://svn.corp.x.com:8080/svn/x/Ontology/tags/
-fi
-
 set -xe
+declare -A repo
+repo=( ["ontology"]="https://svn.corp.x.com:8080/svn/x/Ontology" ["xocl"]="https://svn.corp.x.com:8080/svn/x/xocl" ["xocl-incubator"]="https://svn.corp.x.com:8080/svn/x/xocl")
+REPO="${repo["${PROJ}"]}"
 verify_install
 release
 build_deploy
